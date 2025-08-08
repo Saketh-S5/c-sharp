2e889d4c83078f7f023398635e95c8990d306a4c
Saketh S <saketh.s053@gmail.com>
2025-08-08 14:28:38 +0530

    Add files via upload

---
+++ evenodd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EvenoddApp
+{
+    public class Evenodd
+    {
+        public static void ReadNumber(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("Please enter a non-negative integer.");
+                return;
+            }
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+    }
+}
+        
