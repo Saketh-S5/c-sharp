93b8b4bd4fe37529e09b0e3a7b88f42657b402f9
Saketh S <saketh.s053@gmail.com>
2025-08-12 22:20:33 +0530

    Add files via upload

---
+++ possitivenegetiveorzero/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace possitivenegetiveorzero
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number: ");
+            if (double.TryParse(Console.ReadLine(), out double number))
+            {
+                if (number > 0)
+                    Console.WriteLine($"{number} is positive.");
+                else if (number < 0)
+                    Console.WriteLine($"{number} is negative.");
+                else
+                    Console.WriteLine("The number is zero.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid numeric value.");
+            }
+        }
+    }
+}
