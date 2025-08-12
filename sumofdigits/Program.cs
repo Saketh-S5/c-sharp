c76ee37fcf31680ac51803deba5971d4df6341ef
Saketh S <saketh.s053@gmail.com>
2025-08-12 22:22:45 +0530

    Add files via upload

---
+++ sumofdigits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter an integer: ");
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                int n = Math.Abs(num);
+                int sum = 0;
+                while (n > 0)
+                {
+                    sum += n % 10;
+                    n /= 10;
+                }
+                Console.WriteLine($"Sum of digits: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
+
