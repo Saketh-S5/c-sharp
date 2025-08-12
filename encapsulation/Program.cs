12fc71c0a6c2d76d1b2ce6806f6262512792e8ae
Saketh S <saketh.s053@gmail.com>
2025-08-12 23:15:22 +0530

    Add files via upload

---
+++ encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person1 = new Person();
+
+            Person1.Name = "saketh";
+            Person1.Age = 20;
+            Console.WriteLine($"Name: {Person1.Name} ; Age: {Person1.Age}");
+        }
+    }
+}
