730215175c5034fa27d9cf97c87644496e29e551
Saketh S <saketh.s053@gmail.com>
2025-10-06 20:04:43 +0530

    Add files via upload

---
+++ vehicledemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace vehicledemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle("Model X", "Tesla", 99.99);
+            vehicle.display();
+
+            Car car = new Car("Civic", "Honda", 79.99, 5, "Petrol");
+            car.display();
+        }
+    }
+}
